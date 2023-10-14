1ed4ec99707a0ef3ff5cc84ac90b6585d384fc02
miqueias.ramos <miqueiasrsousabike@gmail.com>
2023-10-13 22:08:36 -0300

    I added a section and JavaScript to the project.

---
+++ script.js
@@ -0,0 +1,12 @@
+
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.presentation', {duration: 1000})
+
+sr.reveal('.size-fullstack', {duration: 1200})
+
+sr.reveal('.size-developer', {duration: 1500})
+
+sr.reveal('.size-info', {duration: 1800})
+
+sr.reveal('.conhecaProjetos', {duration: 2000});
