771a864492b525a95a8c830dfa7ac8007c759e2d
miqueias.ramos <miqueiasrsousabike@gmail.com>
2023-10-19 15:23:09 -0300

    I added about me image, and in the about me

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 
 window.sr = ScrollReveal({ reset: true});
 
-sr.reveal('.presentation', {duration: 1000})
 
-sr.reveal('.size-fullstack', {duration: 1200})
 
-sr.reveal('.size-developer', {duration: 1500})
-
-sr.reveal('.size-info', {duration: 1800})
-
-sr.reveal('.conhecaProjetos', {duration: 2000});
+sr.reveal('.discoverProjects', {duration: 2000});
